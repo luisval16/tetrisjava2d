a3519511143a0c821da1a2bf620369ab5af3b223
dell <dell@dell-PC>
2017-11-27 11:43:36 -0600

    tetris java 2d

---
+++ src/tetris/Tetris.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetris;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+/**
+ *
+ * @author dell
+ */
+public class Tetris extends JFrame{
+     JLabel statusbar;
+
+
+    public Tetris() {
+
+        statusbar = new JLabel(" 0");
+        add(statusbar, BorderLayout.SOUTH);
+        Board board = new Board(this);
+        add(board);
+        board.start();
+
+        setSize(200, 400);
+        setTitle("Tetris");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+   }
+
+   public JLabel getStatusBar() {
+       return statusbar;
+   }
+
+    public static void main(String[] args) {
+
+        Tetris game = new Tetris();
+        game.setLocationRelativeTo(null);
+        game.setVisible(true);
+
+    } 
+}
